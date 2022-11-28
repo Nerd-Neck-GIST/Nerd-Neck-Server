a5e1ed94329213931100c08101ff0cb7b1206559
seunghoon-choi <chltmdwkd@gmail.com>
2022-11-28 19:14:33 +0900

    basic webrtc video test

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,47 +1,81 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+# from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
+import asyncio
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = 'Test-Room'
 
-        # "room" 그룹에 가입
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        print('Disconnected!')
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        peer_username = text_data_json['peer']
+        action = text_data_json['action']
         message = text_data_json['message']
 
-        # "room" 그룹에 메시지 전송
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
+        if(action == 'new-offer') or (action =='new-answer'):
+            # in case its a new offer or answer
+            # send it to the new peer or initial offerer respectively
+
+            receiver_channel_name = text_data_json['message']['receiver_channel_name']
+
+            print('Sending to ', receiver_channel_name)
+
+            text_data_json['message']['receiver_channel_name'] = self.channel_name
+
+            # "room" 그룹에 메시지 전송
+            await self.channel_layer.send(
+                receiver_channel_name,
+                {
+                    'type': 'send.sdp',
+                    'receive_dict': text_data_json,
+                }
+            )
+            return
+        
+        # set new receiver as the current sender
+        # so that some messages can be sent
+        # to this channel specifically
+        text_data_json['message']['receiver_channel_name'] = self.channel_name
+
+        # send to all peers
+        await self.channel_layer.send(
+            receiver_channel_name,
             {
-                'type': 'chat_message',
-                'message': message
+                'type': 'send.sdp',
+                'receive_dict': text_data_json,
             }
         )
-        # "room" 그룹에서 메시지 전송
 
-    def chat_message(self, event):
-        message = event['message']
+
+        # "room" 그룹에서 메시지 전송
+    async def send_sdp(self, event):
+        receive_dict = event['receive_dict']
+
+        this_peer = receive_dict['peer']
+        action = receive_dict['action']
+        message = receive_dict['message']
 
         # 웹 소켓으로 메시지 전송
-        self.send(text_data=json.dumps({
-            'message': message
+        await self.send(text_data=json.dumps({
+            'peer': this_peer,
+            'action': action,
+            'message': message,
         }))
 
 # class ChatConsumer(WebsocketConsumer):
--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,9 +1,12 @@
 # chatapp/urls.py
 from django.urls import path
 
-from . import views
+from .views import peer1, peer2, peer
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    # path('', views.index, name='index'),
+    # path('<str:room_name>/', views.room, name='room'),
+    path('', peer, name='peer'),
+    path('peer1/', peer1, name='peer1'),
+    path('peer2/', peer2, name='peer2'),
 ]
