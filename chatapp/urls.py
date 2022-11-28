9137f7917b2b8166c528bca7135906cd8b56e25b
seunghoon-choi <63503352+seunghoon-choi@users.noreply.github.com>
2022-11-28 19:21:36 +0900

    Revert "basic webrtc video test"

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,12 +1,9 @@
 # chatapp/urls.py
 from django.urls import path
 
-from .views import peer1, peer2, peer
+from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('<str:room_name>/', views.room, name='room'),
-    path('', peer, name='peer'),
-    path('peer1/', peer1, name='peer1'),
-    path('peer2/', peer2, name='peer2'),
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,18 +1,10 @@
+# chatapp/views.py
 from django.shortcuts import render
 
-# def index(request):
-#     return render(request, 'chatapp/index.html')
+def index(request):
+    return render(request, 'chatapp/index.html')
 
-def peer1(request):
-    return render(request, 'chatapp/peer1.html')
-
-def peer2(request):
-    return render(request, 'chatapp/peer2.html')
-
-def peer(request):
-    return render(request, 'chatapp/peer.html')
-
-# def room(request, room_name):
-#     return render(request, 'chatapp/room.html', {
-#         'room_name': room_name
-#     })
+def room(request, room_name):
+    return render(request, 'chatapp/room.html', {
+        'room_name': room_name
+    })
