5a1b57efad115a60e06ade7440a3cd9ed0277097
seunghoon-choi <chltmdwkd@gmail.com>
2022-12-02 03:36:51 +0900

    second webrtc video test

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 from .views import peer1, peer2, peer
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('<str:room_name>/', views.room, name='room'),
     path('', peer, name='peer'),
     path('peer1/', peer1, name='peer1'),
     path('peer2/', peer2, name='peer2'),
